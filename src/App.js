fb8c7b48cd53eb912296e042aeb10170353d505a
dongheon lee <niche3600@gmail.com>
2020-10-16 08:20:55 +0900

    first hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [double, setDouble] = useState(1);
+  useEffect(() => {
+    document.title = `you clicked ${count} times`;
+  }, [double])
+  useEffect(() => {
+    console.log(`you clicked ${double} times`);
+  })
+
+  const addTranslated = (count) => {
+    return `${count} 만큼 눌렀습니다.`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Text>You clicked {count} times</Text>
+      <Text>{addTranslated(count)}</Text>
+      <Text>double count: {double}</Text>
+      <Button onClick={() => setCount(count + 1)}>click!</Button>
+      <Button onClick={() => setDouble(double * 2)}>double!</Button>
+      <progress id="file" value={count} max="100"></progress>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Text = styled.p`
+`;
+
+const Button = styled.button`
+
+`;
 
 export default App;
