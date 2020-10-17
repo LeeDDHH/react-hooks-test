4bca0172c997cf4b6c112c1f982fb740ec3b2333
dongheon lee <niche3600@gmail.com>
2020-10-18 07:28:00 +0900

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Const from './Const';
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [double, setDouble] = useState(1);
+  const [maxValue, setMaxValue] = useState(Const.initMax);
+
+  const reachedMaxValue = (count, maxValue) => {
+    if (maxValue <= count) return true;
+  };
+
   useEffect(() => {
     document.title = `you clicked ${count} times`;
-  }, [double])
+  }, [count]);
+
   useEffect(() => {
     console.log(`you clicked ${double} times`);
-  })
+  });
+  useEffect(() => {
+    if (reachedMaxValue(count, maxValue)) {
+      setMaxValue(maxValue * 1.5);
+      setCount(0);
+    }
+  }, [count, maxValue]);
 
   const addTranslated = (count) => {
-    return `${count} 만큼 눌렀습니다.`
-  }
+    return `${count} 만큼 눌렀습니다.`;
+  };
 
   return (
     <Wrapper>
@@ -22,7 +36,7 @@ const App = () => {
       <Text>double count: {double}</Text>
       <Button onClick={() => setCount(count + 1)}>click!</Button>
       <Button onClick={() => setDouble(double * 2)}>double!</Button>
-      <progress id="file" value={count} max="100"></progress>
+      <progress id="file" value={count} max={maxValue}></progress>
     </Wrapper>
   );
 }
---
+++ src/Const.js
@@ -0,0 +1,5 @@
+export default class Const {
+  static get initMax() {
+    return 10;
+  }
+}
